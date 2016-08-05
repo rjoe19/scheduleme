a52341f4d9a8b6e3be6be5ecfea80b2a7f37e9d2
Richard Joe <rjoe19@hotmail.com>
2016-08-05 18:15:38 +1200

    firebase setup

---
+++ app.js
@@ -0,0 +1,49 @@
+/ create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+
+  // connect to firebase
+var ref = new Firebase("https://burning-torch-4263.firebaseio.com/days");
+var fb = $firebase(ref);
+
+});
+
+
+
+  // function to set the default data
+  $scope.reset = function() {
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });
+
+  };
+
+});
